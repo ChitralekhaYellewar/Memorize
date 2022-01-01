b00b2845bcffcaad44b4c21f8284fefcc7a6ece5
Chitralekha Yellewar <chitralekha.yellewar@gmail.com>
2022-01-01 14:01:20 +0530

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Chitralekha Yellewar on 01/01/22.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
